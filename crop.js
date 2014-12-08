a112ff16047c2ce5a46d7c9576035395bf948d54
Aleksandras Nelkinas <a.nelkinas@gmail.com>
2014-12-08 10:53:14 +0200

    Bump version to 0.1.10

--- crop.js
+++ crop.js
@@ -2,7 +2,7 @@
  * A standalone JavaScript plugin for cross-browser (including mobile) crop.
  *
  * @name Crop
- * @version 0.1.9
+ * @version 0.1.10
  * @author Aleksandras Nelkinas
  * @license [MIT]{@link http://opensource.org/licenses/mit-license.php}
  *
